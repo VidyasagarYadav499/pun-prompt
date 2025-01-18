3e87e438006ab7d88061f3aef7e5c1001236e879
Vidyasagar Yadav <vidyasagaryadav499@gmail.com>
2025-01-18 21:16:33 +0530

    Initial commit

---
+++ src/main/java/com/example/pun_prompt/PunPromptApplication.java
@@ -0,0 +1,13 @@
+package com.example.pun_prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PunPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PunPromptApplication.class, args);
+	}
+
+}
